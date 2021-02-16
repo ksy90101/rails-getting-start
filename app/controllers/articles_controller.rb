22e6cd86a95950bdce46bbef83ca6152fd687746
ksy90101 <ksy90101@gmail.com>
2021-02-16 13:39:20 +0900

    feat: article 전체 조회 기능 구현

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   private
 
   def article_params
